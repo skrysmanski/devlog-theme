b89f47417c5653af4d9eef4cde0933f33cb62109
Sebastian Krysmanski <noreply@manski.net>
2023-12-03 08:37:45 +0100

    Add initial search

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,4 +31,24 @@ $(() => {
     initAutoMenuClose();
     initReadingProgressBar();
     simplifyTopLinks();
+
+    //
+    // Initializes the search box.
+    //
+    // See: https://pagefind.app/docs/ui/
+    // NOTE: To test this:
+    // 1. Run "hugo" to compile the site
+    // 2. Run "npx pagefind --site "../../../public" (from the assets directory) to compile the search index
+    // 3. Copy the directory "public/pagefind" to the site's "static" directory.
+    new PagefindUI({
+        element: "#search-box",
+        // Don't show multiple results per page.
+        showSubResults: false,
+        // The number of search results to load at once, before a "Load more" button is shown.
+        pageSize: 10,
+        // We don't have images. No need to show any.
+        showImages: false,
+        // Removes any inline styles from the generated HTML elements.
+        resetStyles: false
+    });
 });
