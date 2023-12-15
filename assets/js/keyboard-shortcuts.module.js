7e37070e84e2cd389a0e4247720db818bf84fd52
Sebastian Krysmanski <noreply@manski.net>
2023-12-15 12:31:16 +0100

    Drop jquery from keyboard-shortcuts module

--- assets/js/keyboard-shortcuts.module.js
+++ assets/js/keyboard-shortcuts.module.js
@@ -1,11 +1,9 @@
-import $ from "./jquery.module.js";
-
 export const registerGlobalKeyboardShortcut = (key, eventHandler, options = null) => {
     const requireCtrl = options?.requireCtrl === true;
     const requireShift = options?.requireShift === true;
     const requireMeta = options?.requireMeta === true;
 
-    $(document).on('keyup', (event) => {
+    document.addEventListener('keyup', (event) => {
         if (event.isComposing) {
             // Ignore CJKT composing.
             // See: https://developer.mozilla.org/en-US/docs/Web/API/Element/keyup_event
