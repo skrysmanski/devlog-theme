2abc675ec7c6149800af01fba804f5ab811763cb
Sebastian Krysmanski <noreply@manski.net>
2023-12-20 05:52:19 +0100

    Add create-search-index.ps1 script

---
+++ assets/create-search-index.ps1
@@ -0,0 +1,14 @@
+#!/usr/bin/env pwsh
+
+$rootDir = [IO.Path]::GetFullPath("$PSScriptRoot/../../..")
+Write-Host "Using Hugo files from: $rootDir"
+
+$pageFindDir = "$rootDir/static/pagefind"
+Remove-Item $pageFindDir -Recurse
+
+$tempOutputDir = [IO.Path]::GetFullPath("$PSScriptRoot/../.publish-temp")
+
+& hugo --gc --cleanDestinationDir --source $rootDir --destination $tempOutputDir
+
+# See: https://pagefind.app/docs/running-pagefind/
+& npx pagefind --site $tempOutputDir --output-path $pageFindDir
