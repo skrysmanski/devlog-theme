82ce68ece1bda21650a739d2749edd4661075793
Sebastian Krysmanski <noreply@manski.net>
2024-02-12 17:28:13 +0100

    Change script param to root folder

--- _utils/beautify.js
+++ _utils/beautify.js
@@ -1,14 +1,18 @@
 const { glob } = require('glob');
 const fs = require('fs').promises;
+const path = require('path');
 const beautify = require('js-beautify');
 
-const pathPattern = process.argv[2];
-if (!pathPattern) {
-    throw 'Missing parameter';
-}
-
 (async () => {
-    const filePaths = await glob(pathPattern);
+    let rootPath = process.argv[2];
+    if (!rootPath) {
+        throw 'Missing parameter';
+    }
+
+    rootPath = path.resolve(rootPath);
+    console.log(`Beautifying files in: ${rootPath}`)
+
+    const filePaths = await glob(`${rootPath}/**/*.html`);
 
     const beautifyPromises = filePaths.map(async (filePath) => {
          const fileContents = await fs.readFile(filePath, { encoding: 'utf8' });
