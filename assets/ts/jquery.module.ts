ee280fa9bbc04d59c1110ff707027871e63a32ed
Sebastian Krysmanski <noreply@manski.net>
2024-02-16 15:14:23 +0100

    Load jQuery separately (and switch to jQuery slim)
    This allows us to keep the bulk of the JS size minified (253kb)

--- assets/ts/jquery.module.ts
+++ assets/ts/jquery.module.ts
@@ -1,4 +1,6 @@
+// NOTE: We don't import jQuery here because we want to keep it separate from our own code. This way
+//   we can use the minified jQuery version but still keep our own JavaScript/TypeScript code un-minified (for readability).
 // @ts-ignore
-import $ from "../node_modules/jquery/dist/jquery.js";
+//import $ from "../node_modules/jquery/dist/jquery.js";
 
 export default <JQueryStatic>$.noConflict(true);
